8d9694e05e60b5c7f736b93505b34ecbe5ef6bfb
dima5fahrul <dfpa.05@gmail.com>
2022-02-03 22:44:09 +0700

    baru upload

---
+++ js.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+window.addEventListener('scroll', function() {
+	var navbar = document.querySelector('nav');
+	navbar.classList.toggle('sticky', window.scrollY > 0);
+	if (window.scrollY <= 0) {
+		navbar.classList.add('py-4', window.scrollY > 0);
+		navbar.classList.remove('py-1', window.scrollY > 0);
+	} else {
+		navbar.classList.add('py-1', window.scrollY > 0);
+		navbar.classList.remove('py-4', window.scrollY > 0);
+	}
+});
+
+window.addEventListener('resize', function() {
+	var responsive1 = document.querySelector('.skills .col-lg-8');
+	var responsive2 = document.querySelector('.skills .col-lg-8 img');
+	
+	responsive1.classList.toggle('d-flex', window.innerWidth > 992);
+	responsive2.classList.toggle('d-block', window.innerWidth > 992);
+});
+
+window.addEventListener('load', function() {
+	var responsive1 = document.querySelector('.skills .col-lg-8');
+	var responsive2 = document.querySelector('.skills .col-lg-8 img');
+	
+	responsive1.classList.toggle('d-flex', window.innerWidth > 992);
+	responsive2.classList.toggle('d-block', window.innerWidth > 992);
+});
+
+
+
+
+//Aos
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
